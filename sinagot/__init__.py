e6b01eda4a085140c8f8a1930fdbb6bd1df7b3dd
Yann Beauxis <pro@yannbeauxis.net>
2020-09-17 11:07:54 +0200

    debug __version__

--- sinagot/__init__.py
+++ sinagot/__init__.py
@@ -1,11 +1,8 @@
-from pathlib import Path
-import toml
+import pkg_resources
 from sinagot.models import Workspace
 from .script import ScriptTemplate
 
 
 __all__ = ["Workspace", "ScriptTemplate"]
 
-conf_path = Path(__file__).parents[1] / "pyproject.toml"
-conf = toml.loads(conf_path.read_text())
-__version__ = conf["tool"]["poetry"]["version"]
+__version__ = pkg_resources.get_distribution("sinagot").version
