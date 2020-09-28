b8bca621aa320b1a01c6129d4911c40a2e2450ee
Yann Beauxis <dev@yannbeauxis.net>
2020-09-28 16:36:44 +0200

    build on for docs requirements

--- update_docs_requirements.sh
+++ update_docs_requirements.sh
@@ -1,4 +1,10 @@
-REQUIREMENTS_PATH=docs/requirements.txt
+DOCS_PATH=docs
+REQUIREMENTS_PATH=$DOCS_PATH/requirements.txt
+SINAGOT_VERSION=$(poetry run python -c 'import sinagot; print(sinagot.__version__)')
+SINAGOT_BUILD=sinagot-$SINAGOT_VERSION.tar.gz
 
+poetry build
+rm $DOCS_PATH/sinagot-*.whl
+cp dist/$SINAGOT_BUILD docs/$SINAGOT_BUILD
 poetry export -f requirements.txt --dev --without-hashes -o $REQUIREMENTS_PATH
-echo "sinagot==$(poetry run python -c 'import sinagot; print(sinagot.__version__)')" >> $REQUIREMENTS_PATH
+echo "./docs/$SINAGOT_BUILD" >> $REQUIREMENTS_PATH
